1fc249819f64ca2e2acf2df95f85e123d0f9103d
Melanie Fryman <melanie@frymanet.com>
2014-07-30 12:08:34 -0500

    add data via dynamic url

--- app/index.js
+++ app/index.js
@@ -9,8 +9,9 @@ app.set('view engine', 'ejs'); // configures express server as the view engine
 app.set('views', __dirname + '/views'); // configures express server
 
 app.use(morgan('dev')); // first thing a request does: log request
-app.use(express.static(__dirname + '/static')); // looks to see if request can be met in static dir first
+app.use(express.static(__dirname + '/static')); // looks to see if request can be met in static dir first; static routes
 
+// 'app.get' sets your dynamic routes
 app.get('/', function(req, res){ // if not in static, then looks under / dir.
   res.render('index');
 });
@@ -19,10 +20,18 @@ app.get('/checkers', function(req, res){ // if not in home dir, then looks for m
   res.render('checkers'); // renders view file 'checkers'
 });
 
+app.get('/add/:a/:b/:c/:d', function(req, res){
+  req.params.a *= 1;
+  req.params.b *= 1;
+  req.params.c *= 1;
+  req.params.d *= 1;
+  res.render('sum', req.params);
+});
+
 var port = process.env.PORT; // creates variable to dynamically assign PORT.  
 
 // dynamically assign PORT by starting 'supervisor' in the command line
 
 app.listen(port, function(){ //tells Express to listen for http requests on PORT x.
-  console.log('Express.js is listening, on PORT', process.env.PORT);
+  console.log('Express.js is listening, on PORT' + port);
 });
